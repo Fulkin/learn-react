6293651bdf1079dbd89b91f704df058da9acbf1e
Evgeniy <sea-xv@mail.ru>
2025-12-22 19:38:43 +0300

    hw-9 change after review

--- src/components/restaurant/restaurant-container.jsx
+++ src/components/restaurant/restaurant-container.jsx
@@ -3,12 +3,12 @@ import {selectRestaurantById} from "../../redux/entity/restaurant/slice.js";
 import {Restaurant} from "./restaurant.jsx";
 import {Outlet} from "react-router";
 import {useRequest} from "../../redux/hooks/use-request.js";
-import {getRestaurants} from "../../redux/entity/restaurant/get-restaurants.js";
+import {getRestaurantById} from "../../redux/entity/restaurant/get-restaurant-by-id.js";
 
 export const RestaurantContainer = ({restaurantId}) => {
     const restaurant = useSelector((state) => selectRestaurantById(state, restaurantId));
 
-    const {isLoading, isError} = useRequest(getRestaurants);
+    const {isLoading, isError} = useRequest(getRestaurantById, restaurantId);
 
     if (isLoading) {
         return "loading...";
